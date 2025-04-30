1e716057023ac293b326788eca69ade6af0006ea
Khadija025 <124507603+Khadija025@users.noreply.github.com>
2025-04-30 11:11:56 +0300

    Add files via upload

---
+++ ArrayApplicationDemo/src/main/java/com/example/arrayapplicationdemo/ArrayApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.arrayapplicationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrayApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrayApplicationDemoApplication.class, args);
+	}
+
+}
---
+++ ArrayApplicationDemo/src/test/java/com/example/arrayapplicationdemo/ArrayApplicationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arrayapplicationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrayApplicationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
